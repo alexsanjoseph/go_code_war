485a680d66ca311a48e98f44fc38f062df9dea35
alexsanjoseph <alexsanjoseph@gmail.com>
2021-11-26 22:14:55 +0530

    chess game

--- kata_test.go
+++ kata_test.go
@@ -13,26 +13,45 @@ func TestKata(t *testing.T) {
 	RunSpecs(t, "Books Suite")
 }
 
-func dotest(a1 string, exp string) {
-	var ans = Stati(a1)
+func dotest(n int, exp []int) {
+	var ans = Game(n)
 	Expect(ans).To(Equal(exp))
 }
 
-var _ = Describe("Tests Stati", func() {
+var _ = Describe("Test Example", func() {
 
 	It("should handle basic cases", func() {
-		dotest("01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17",
-			"Range: 01|01|18 Average: 01|38|05 Median: 01|32|34")
-		dotest("02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41",
-			"Range: 00|31|17 Average: 02|26|18 Median: 02|22|00")
-
-	})
-
-	It("should convert to second correctly", func() {
-		Expect(ConvertToSeconds("01|15|59")).To(Equal(4559))
-		Expect(ConvertToString(4559)).To(Equal("01|15|59"))
+		dotest(0, []int{0})
+		dotest(1, []int{1, 2})
+		dotest(2, []int{2})
+		dotest(3, []int{9, 2})
+		dotest(4, []int{8})
+		dotest(5, []int{25, 2})
+		dotest(8, []int{32})
+		dotest(40, []int{800})
 	})
 })
+
+// func dotest(a1 string, exp string) {
+// 	var ans = Stati(a1)
+// 	Expect(ans).To(Equal(exp))
+// }
+
+// var _ = Describe("Tests Stati", func() {
+
+// 	It("should handle basic cases", func() {
+// 		dotest("01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17",
+// 			"Range: 01|01|18 Average: 01|38|05 Median: 01|32|34")
+// 		dotest("02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41",
+// 			"Range: 00|31|17 Average: 02|26|18 Median: 02|22|00")
+
+// 	})
+
+// 	It("should convert to second correctly", func() {
+// 		Expect(ConvertToSeconds("01|15|59")).To(Equal(4559))
+// 		Expect(ConvertToString(4559)).To(Equal("01|15|59"))
+// 	})
+// })
 
 // func dotest(k, start, nd int, exp []int) {
 // 	var ans = Step(k, start, nd)
--- kyu6/chess_game.go
+++ kyu6/chess_game.go
@@ -1,1 +1,60 @@
 package kata
+
+import "fmt"
+
+func GCD(a, b int64) int64 {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func LCM(a, b int64, integers ...int64) int64 {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
+
+func SumFraction(num1, denom1, num2, denom2 int64) (int64, int64) {
+	if num1 == 0 {
+		return num2, denom2
+	}
+	if num2 == 0 {
+		return num1, denom1
+	}
+	lcmVal := LCM(denom1, denom2)
+	num, denom := (num1*lcmVal/denom1)+(num2*lcmVal/denom2), lcmVal
+	gcdVal := GCD(num, denom)
+
+	return num / gcdVal, denom / gcdVal
+}
+
+func Game2(n int) []int {
+	fmt.Println("START")
+	if n == 0 {
+		return []int{0}
+	}
+	num, denom := int64(0), int64(1)
+	for i := int64(0); i < int64(n); i++ {
+		for j := int64(1); j <= int64(n); j++ {
+			num, denom = SumFraction(i+1, i+j+1, num, denom)
+		}
+	}
+	if denom == 1 {
+		return []int{int(num)}
+	}
+	return []int{int(num), int(denom)}
+}
+
+func Game(n int) []int {
+	if n%2 == 1 {
+		return []int{n * n, 2}
+	}
+	return []int{n * n / 2}
+}
